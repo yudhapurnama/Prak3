ab2f5a192adbf318f5f17a92a179832624164721
NARJO <yudhapurnamaputra52@gmail.com>
2024-10-21 20:25:55 +0700

    first commit

---
+++ praktikum3.py
@@ -0,0 +1,13 @@
+print("program Python Hitung Luas dan Keliling Lingkaran")
+print("Nama : Muhammad Din Al Ayubi")
+print("NIM : 312210293")
+print("Kelas : T.I 22 A.3")
+
+import math
+r = float(input("Masukkan jari- jari : "))
+luas = math.pi*r
+keliling = 2*math.pi*r
+
+print("\n----------Hasilnya----------")
+print("Luas Lingkaran\t\t =",Luas)
+print("Keliling Lingkaran\t =" ,keliling)
